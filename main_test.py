75d61bfe49ae7c4e6314c597c360fd49d9ef4ee4
Eduardo <dudarenz@gmail.com>
2023-03-28 23:16:37 -0300

    test a wrong thing

--- main_test.py
+++ main_test.py
@@ -8,5 +8,8 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(currency_format('10.45'), 'R$ 10,45')
         self.assertEqual(currency_format('10,45'), 'R$ 10,45')
 
+    def test_should_wrong(self):
+        self.assertEqual(currency_format('10'), 'R$ 10.00')
+
 if __name__ == '__main__':
     unittest.main()

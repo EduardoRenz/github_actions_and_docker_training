ca9c01214e767c19b60fc5701cd2b34fd0bf976b
Eduardo Renz <dudarenz@gmail.com>
2023-04-13 21:21:13 -0300

    created route for root

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return { "message": "This is root"}
 
 @app.route("/format/<number>")
-def hello_world(number):
+def format(number):
     return { "formated": currency_format(number)}
 
